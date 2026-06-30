de1db9899972f69f36ca3573c1d695679dd673de
ANKIT <sycocoders@gmail.com>
2026-06-30 12:37:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,40 @@
-# Crypto-Tracker-
-Crypto Tracker is a modern web application built with Flask that provides real-time cryptocurrency prices, market trends, and detailed coin information using the CoinGecko API. It features a responsive UI, live price updates, search functionality, and a clean dashboard for tracking popular cryptocurrencies.
+from flask import Flask, render_template
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+BASE_URL = os.getenv("COINGECKO_API")
+
+
+@app.route("/")
+def home():
+    try:
+        url = f"{BASE_URL}/coins/markets"
+        params = {
+            "vs_currency": "usd",
+            "order": "market_cap_desc",
+            "per_page": 20,
+            "page": 1,
+            "sparkline": "false"
+        }
+
+        response = requests.get(url, params=params, timeout=10)
+        coins = response.json()
+
+    except Exception:
+        coins = []
+
+    return render_template("index.html", coins=coins)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
